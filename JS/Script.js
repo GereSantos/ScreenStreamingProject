dccd0c5f5d541e09dce7bb03c4412aa04a4ed8b0
Geremias Santos <Geremias99999@gmail.com>
2022-09-09 15:09:54 -0300

    NewScreens

---
+++ JS/Script.js
@@ -0,0 +1,11 @@
+function goSeries(){
+    window.location.href = "/HTML/Séries.html";
+}
+
+function goFilmes(){
+    window.location.href = "/HTML/Filmes.html";
+}
+
+function goDocument(){
+    window.location.href = "/HTML/Documentario.html";
+}
